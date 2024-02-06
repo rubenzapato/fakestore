680a17e9e8905086a9cf1b30b5fc71635a8464ae
rubenzapato <128716240+rubenzapato@users.noreply.github.com>
2024-02-05 20:26:07 -0600

    second commit

--- src/components/SingleProduct/index.jsx
+++ src/components/SingleProduct/index.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import {  useParams } from 'react-router-dom';
+import useApi from '../../hooks/useApi';
 
 const SingleProduct = () => {
-    const location = useLocation();
     const { id } = useParams();
-    if (!location.state) {
-        return <div>No hay información del artículo</div>;
-      }
-    const { title, price, image, category, description } = location.state;
-    return ( 
-        <div className="App">
-            <section className='singleProduct'>
-                <h2>Detalles del artículo {id}</h2>
-               <p>{title}</p>
-                <p>Price: {price}</p>
-                <p>Image: {image}</p>
-                <p>Category: {category}</p>
-                <p>{description}</p>
-            </section>
-            
+    let urlArticle= ` https://fakestoreapi.com/products/${id}`
+    //const { title, price, image, category, description } = location.state;
+    const { data, loading, error } = useApi(urlArticle)
+    console.log(data)
 
+    return ( 
+        
+            <div className='App'>
+            <section className='singleProduct'>
+
+                <h2>{data.title}</h2>
+                <h3>${data.price}</h3>
+                <img src={data.image} alt= "Image of a product"/>
+                <h4>Category: {data.category}</h4>
+                <p>{data.description}</p>
+                 
+            </section>
         </div>
+        
+  
      );
 }
  
